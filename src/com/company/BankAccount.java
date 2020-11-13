c362749147e96e8d7521402e511b9367dd2ac129
okot444 <okot444@gmail.com>
2020-11-13 17:46:29 +0300

    first commit, basic classes

---
+++ src/com/company/BankAccount.java
@@ -0,0 +1,51 @@
+package com.company;
+
+import java.util.Date;
+enum CoinType{
+    Dollar,
+    Ruble,
+    Euro
+}
+
+public class BankAccount {
+    
+
+    private Date date;
+    private Double Balance = 0.0;
+    private Integer ID;
+    private String CoinType;
+
+    public Double getBalance() {
+        return Balance;
+    }
+
+    public void setBalance(Double balance) {
+        Balance = balance;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public String getCoinType() {
+        return CoinType;
+    }
+
+    public Integer getID() {
+        return ID;
+    }
+
+    BankAccount(String type){
+        date = new Date();
+        CoinType = type;
+        ID =
+    }
+
+    public void Deposit(Double Sum){
+        setBalance(Sum + getBalance());
+    }
+
+
+
+
+}
